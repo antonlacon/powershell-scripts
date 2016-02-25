f53094a2198ea97506f11e165a4a6df32ccc3e1a
Ian Leonard <antonlacon@gmail.com>
2016-02-24 21:27:50 -0800

    CompressDirectory.ps1: Avoid recompressing already compressed directories
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- CompressDirectory.ps1
+++ CompressDirectory.ps1
@@ -31,7 +31,7 @@
 
 #>
 
-### Setup Variables ###
+### SETUP VARIABLES ###
 
 # Directories to compress
 $Steam_Game_Directory = 'C:\Program Files (x86)\Steam\SteamApps\common'
@@ -66,7 +66,30 @@ Add-DirectoryToList $Steam_Game_Directory $Origin_Game_Directory
 # Compress each directory in turn
 if ( $List_Of_Directories.length -ne 0 ) {
     Foreach( $Directory in $List_Of_Directories ) {
-        Compact /c /exe:$Compression_Method /s:$Directory
+        # Compact's query is verbose; only interested in the 3rd to last line of output
+        $Directory_Status=@(Compact /q /s:$Directory | Select -Last 3 | Select -First 1 )
+        $Directory_Status=$Directory_Status -Split '\s+'
+
+        # If sizes match, data is uncompressed; proceed with compressing
+        # First(0) 'word' is Data Payload Size. Ninth(8) 'word' is Size on Disk.
+        if ( $Directory_Status[0] -eq $Directory_Status[8] ) {
+            Compact /c /exe:$Compression_Method /s:$Directory
+        }
+        # Otherwise some data is compressed; check for uncompressed subdirectories
+        else {
+            $List_of_Subdirectories=@( Get-ChildItem -Directory -Name $Directory )
+            Foreach( $Subdirectory in $List_of_Subdirectories ) {
+
+                # Query each subdirectory to determine if it is already compressed
+                $Query_Output=@( Compact /q /s:"$Directory\$Subdirectory" | Select -Last 3 | Select -First 1 )
+                $Query_Output=$Query_Output -Split '\s+'
+
+                # If sizes match, data is uncompressed; proceed with compressing
+                if ( $Query_Output[0] -eq $Query_Output[8] ) {
+                    Compact /c /exe:$Compression_Method /s:"$Directory\$Subdirectory"
+                }
+            }
+        }
     }
 }
 else {
